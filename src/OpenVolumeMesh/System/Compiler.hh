0d447e1d49db84ad3b8d257f1fb71d61b48d710b
Martin Heistermann <git@mheistermann.de>
2019-08-19 19:09:37 +0200

    Create c++ version detection macros OVM_CXX_{14,17}.

---
+++ src/OpenVolumeMesh/System/Compiler.hh
@@ -0,0 +1,11 @@
+#if __cplusplus >= 201402L
+    #define OVM_CXX_14 1
+#else
+    #define OVM_CXX_14 0
+#endif
+
+#if __cplusplus >= 201703L
+    #define OVM_CXX_17 1
+#else
+    #define OVM_CXX_17 0
+#endif
