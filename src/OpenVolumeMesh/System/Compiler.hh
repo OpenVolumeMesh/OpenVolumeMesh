c0570165a1911d36618abc8d3b437d94ed76b52d
Martin Heistermann <git@mheistermann.de>
2019-08-19 20:24:32 +0200

    Determine C++ version at library configure time, not at client build time.

--- src/OpenVolumeMesh/System/Compiler.hh
+++ src/OpenVolumeMesh/System/Compiler.hh
@@ -1,10 +1,6 @@
-#if __cplusplus >= 201402L
-    #define OVM_CXX_14 1
-#else
-    #define OVM_CXX_14 0
-#endif
+#include "../Config/Version.hh"
 
-#if __cplusplus >= 201703L
+#if OPENVOLUMEMESH_CXX_VERSION >= 17
     #define OVM_CXX_17 1
 #else
     #define OVM_CXX_17 0
