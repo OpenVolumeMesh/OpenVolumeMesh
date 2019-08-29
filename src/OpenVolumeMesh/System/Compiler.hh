0ad436fee642764f9eb99cf40147b4464674cb65
Martin Heistermann <git@mheistermann.de>
2019-08-29 13:37:34 +0200

    compiler.hh: provide defines for c++ version at build time and (client) compile time

--- src/OpenVolumeMesh/System/Compiler.hh
+++ src/OpenVolumeMesh/System/Compiler.hh
@@ -1,6 +1,18 @@
 #include "../Config/Version.hh"
 
+// C++ version at OVM build time,
+// to be used for things that change ABI
+
 #if OPENVOLUMEMESH_CXX_VERSION >= 17
+    #define OVM_BUILD_CXX_17 1
+#else
+    #define OVM_BUILD_CXX_17 0
+#endif
+
+// C++ version when compiling (e.g. client code),
+// be careful not to change ABI depending on these defines.
+
+#if __cplusplus >= 201703L
     #define OVM_CXX_17 1
 #else
     #define OVM_CXX_17 0
