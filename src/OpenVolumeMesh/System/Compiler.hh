bd6b72c69ebdfda5829b79b4ad0a4cfe32ac64a7
Martin Heistermann <git@mheistermann.de>
2019-10-04 12:18:52 +0200

    cmake: do not set cxx standard on indidual projects, to avoid mixing versions

--- src/OpenVolumeMesh/System/Compiler.hh
+++ src/OpenVolumeMesh/System/Compiler.hh
@@ -1,14 +1,5 @@
 #include "OpenVolumeMesh/Config/Version.hh"
 
-// C++ version at OVM build time,
-// to be used for things that change ABI
-
-#if OPENVOLUMEMESH_CXX_VERSION >= 17
-    #define OVM_BUILD_CXX_17 1
-#else
-    #define OVM_BUILD_CXX_17 0
-#endif
-
 // C++ version when compiling (e.g. client code),
 // be careful not to change ABI depending on these defines.
 
