6b6fbbbf8b71d9955d119ad7c02955ab83c68f73
Martin Heistermann <git@mheistermann.de>
2022-02-11 16:40:58 +0100

    Bugfix: swapping entities did not swap bool properties correctly.

--- src/OpenVolumeMesh/Core/Properties/PropertyStorageT.hh
+++ src/OpenVolumeMesh/Core/Properties/PropertyStorageT.hh
@@ -232,7 +232,7 @@ inline void PropertyStorageT<bool>::swap(size_t _i0, size_t _i1)
     // on libstdc++ with _GLIBCXX_DEBUG it doesn't compile
     // (2018-02-26, libstdc++ 8.2.0)
 
-    auto tmp = data_[_i0];
+    bool tmp = data_[_i0];
     data_[_i0] = data_[_i1];
     data_[_i1] = tmp;
 }
