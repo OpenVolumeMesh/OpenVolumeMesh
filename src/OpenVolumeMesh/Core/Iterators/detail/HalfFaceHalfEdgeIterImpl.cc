db3ce77b4bcd1aeed95e8df02d0999f74934cb30
Tobias Kohler <tobias.kohler@unibe.ch>
2025-05-13 17:36:48 +0200

    halfface halfedge and vertex iterator: avoid vector copying

--- src/OpenVolumeMesh/Core/Iterators/detail/HalfFaceHalfEdgeIterImpl.cc
+++ src/OpenVolumeMesh/Core/Iterators/detail/HalfFaceHalfEdgeIterImpl.cc
@@ -5,41 +5,62 @@ namespace OpenVolumeMesh::detail {
 
 HalfFaceHalfEdgeIterImpl::HalfFaceHalfEdgeIterImpl(const HalfFaceHandle& _ref_h, const TopologyKernel* _mesh, int _max_laps) :
     BaseIter(_mesh, _ref_h, _max_laps),
-    halfedges_(_mesh->halfface(_ref_h).halfedges()),
     cur_index_(0)
 {
     assert(halfedges_.size() > 0);
     if (BaseIter::valid()) {
-        BaseIter::cur_handle(halfedges_[cur_index_]);
+        BaseIter::cur_handle(cur_heh());
     }
 }
 
-HalfFaceHalfEdgeIterImpl& HalfFaceHalfEdgeIterImpl::operator--() {
+HalfFaceHalfEdgeIterImpl& HalfFaceHalfEdgeIterImpl::operator--()
+{
     if (cur_index_ == 0) {
-        cur_index_ = halfedges_.size() - 1;
+        cur_index_ = f_hehs().size() - 1;
         --lap_;
-        if (lap_ < 0)
+        if (lap_ < 0) {
             BaseIter::valid(false);
+        }
     }
     else {
         --cur_index_;
     }
-    BaseIter::cur_handle(halfedges_[cur_index_]);
+    BaseIter::cur_handle(cur_heh());
     return *this;
 }
 
-HalfFaceHalfEdgeIterImpl& HalfFaceHalfEdgeIterImpl::operator++() {
+HalfFaceHalfEdgeIterImpl& HalfFaceHalfEdgeIterImpl::operator++()
+{
     ++cur_index_;
-    if (cur_index_ >= halfedges_.size()) {
+    if (cur_index_ >= f_hehs().size()) {
         cur_index_ = 0;
         ++lap_;
-        if (lap_ >= max_laps_)
+        if (lap_ >= max_laps_) {
             BaseIter::valid(false);
+        }
     }
-    BaseIter::cur_handle(halfedges_[cur_index_]);
+    BaseIter::cur_handle(cur_heh());
     return *this;
 }
 
+const std::vector<HalfEdgeHandle>& HalfFaceHalfEdgeIterImpl::f_hehs() const
+{
+    return mesh()->face(ref_handle_.face_handle()).halfedges();
+}
+
+HalfEdgeHandle HalfFaceHalfEdgeIterImpl::cur_heh() const
+{
+    if (ref_handle_.subidx()==1)
+    {
+        // Reversed Face
+        return f_hehs()[f_hehs().size()-1-cur_index_].opposite_handle();
+    }
+    else
+    {
+        // Original Face
+        return f_hehs()[cur_index_];
+    }
+}
 
 
 } // namespace OpenVolumeMesh::detail
