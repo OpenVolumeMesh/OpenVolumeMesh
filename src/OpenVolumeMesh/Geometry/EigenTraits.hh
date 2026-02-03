d395283cadd5b8f1516281075225bbfe6f21906f
Martin Heistermann <git@mheistermann.de>
2026-02-03 15:14:21 +0100

    Eigen support in GeometryKernel and for properties.

--- src/OpenVolumeMesh/Geometry/EigenTraits.hh
+++ src/OpenVolumeMesh/Geometry/EigenTraits.hh
@@ -1,5 +1,7 @@
 #pragma once
 #include <OpenVolumeMesh/Geometry/Vector11T.hh>
+#include <OpenVolumeMesh/Core/Properties/Defaults.hh>
+
 #include <Eigen/Core>
 
 namespace OpenVolumeMesh::Geometry
@@ -12,5 +14,13 @@ struct vector_dim<Eigen::Matrix<_Scalar, 1, _Cols>> {
 template<typename _Scalar, int _Rows>
 struct vector_dim<Eigen::Matrix<_Scalar, _Rows, 1>> {
     constexpr static int dim = _Rows;
+};
+}
+
+namespace OpenVolumeMesh {
+template<typename Scalar, int _Rows, int _Cols>
+struct default_prop<Eigen::Matrix<Scalar, _Rows, _Cols>> {
+    using MatrixT = Eigen::Matrix<Scalar, _Rows, _Cols>;
+    static inline const MatrixT value = MatrixT::Zero();
 };
 }
