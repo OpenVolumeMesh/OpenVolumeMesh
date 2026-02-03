c6b7c6d26c5adc088d57046344c1f4b657dea7e1
Martin Heistermann <git@mheistermann.de>
2026-02-03 19:50:56 +0100

    Eigen support: vector_dim for row and column vectors.

--- src/OpenVolumeMesh/Geometry/EigenTraits.hh
+++ src/OpenVolumeMesh/Geometry/EigenTraits.hh
@@ -5,8 +5,12 @@
 namespace OpenVolumeMesh::Geometry
 {
 
-template<typename Scalar, int DIM>
-struct vector_dim<Eigen::Vector<Scalar, DIM>>
-{ constexpr static int  dim = DIM;};
-
+template<typename _Scalar, int _Cols>
+struct vector_dim<Eigen::Matrix<_Scalar, 1, _Cols>> {
+    constexpr static int dim = _Cols;
+};
+template<typename _Scalar, int _Rows>
+struct vector_dim<Eigen::Matrix<_Scalar, _Rows, 1>> {
+    constexpr static int dim = _Rows;
+};
 }
